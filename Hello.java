e3d3984b5bd1f695b2bc85b8eb9ecaa170e518da
caotd2 <46725055+caotd2@users.noreply.github.com>
2019-01-15 14:16:47 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/**********************************
+* Hello.Java
+***********************************
+*Project/Program description
+*__________________________________
+*Tien Cao
+*1/15/2019
+*255-002
+**********************************/
+
+public class Hello {
+ public static void printHeading() {
+  System.out.println("Tien Cao");
+  System.out.println("Hello Java");
+  System.out.println("Project Description");
+  System.out.println("Version Date");
+  System.out.println("255-002");
+  System.out.println();
+ }
+
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello");
+  }
+ }
